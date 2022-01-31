bc3c9c1d03ec10f65175feb86c8a5caa3f48bb4a
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-01-31 17:07:32 -0500

    Base-Project-Part-II

---
+++ Parte-02/src/test/java/eud/eci/ieti/taskapp/TaskappApplicationTests.java
@@ -0,0 +1,13 @@
+package eud.eci.ieti.taskapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

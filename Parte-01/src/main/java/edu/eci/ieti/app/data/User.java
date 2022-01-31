213fa0fe3703d616936f1bd4cdda69f7f804a103
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-01-31 12:21:09 -0500

    Base-project-Part-I

---
+++ Parte-01/src/main/java/edu/eci/ieti/app/data/User.java
@@ -0,0 +1,15 @@
+package edu.eci.ieti.app.data;
+
+/**
+ * @author Iván Camilo Rincón Saavedra
+ * @version 1.0 1/31/2022
+ * @project app
+ */
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String lastName;
+    private createdAt
+
+}
---
+++ Parte-01/src/main/java/edu/eci/ieti/app/dto/UserDto.java
@@ -0,0 +1,44 @@
+package edu.eci.ieti.app.dto;
+
+/**
+ * @author Iván Camilo Rincón Saavedra
+ * @version 1.0 1/31/2022
+ * @project app
+ */
+public class UserDto {
+    private String name;
+    private String email;
+    private String lastName;
+
+    public UserDto(){}
+
+    public UserDto(String name, String email, String lastName){
+        this.name = name;
+        this.email = email;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}

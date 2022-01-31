7d58a559b55209a9d707525f7946630ef3316f56
Camilo Rincon <ivan.rincon-s@mail.escuelaing.edu.co>
2022-01-31 12:23:57 -0500

    User

--- Parte-01/src/main/java/edu/eci/ieti/app/data/User.java
+++ Parte-01/src/main/java/edu/eci/ieti/app/data/User.java
@@ -10,6 +10,55 @@ public class User {
     private String name;
     private String email;
     private String lastName;
-    private createdAt
+    private String createdAt;
 
+    public User(){}
+
+    public User(int id, String name, String email, String lastName, String createdAt){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.lastName = lastName;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
 }
